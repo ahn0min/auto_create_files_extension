8940823c3fa7a360de3fc9385472af948e18e519
YeongMin Ahn <ahn0min95@gmail.com>
2023-09-28 10:06:49 +0900

    chore: 주석제거

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,8 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
 import * as path from "path";
 import * as fs from "fs";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-
-  console.log(
-    'Congratulations, your extension "snowball-components-folder-template" is now active!'
-  );
-
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  // let disposable = vscode.commands.registerCommand(
-  // "snowball-components-folder-template.helloWorld",
-  // () => {
   const fileSystemWatcher = vscode.workspace.createFileSystemWatcher("**/");
 
   fileSystemWatcher.onDidCreate((event) => {
@@ -51,8 +34,6 @@ export function activate(context: vscode.ExtensionContext) {
   });
 
   context.subscriptions.push(fileSystemWatcher);
-  // context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
